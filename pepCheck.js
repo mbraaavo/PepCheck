be625743f8ffddfb5403bafbf16959edf7533183
Marcos Bravo <76933913+mbraaavo@users.noreply.github.com>
2021-07-29 17:29:42 -0600

    Add files via upload

--- pepCheck.js
+++ pepCheck.js
@@ -1,3 +1,98 @@
+//This function is meant to create each pepper as an object
+
+function pepper(pep_Name, nicknames, origin, sco_Min, sco_Max, heat, img) {
+  this.pep_Name = pep_Name;
+  this.nicknames = nicknames;
+  this.origin = origin;
+  this.sco_Min = sco_Min;
+  this.sco_Max = sco_Max;
+  this.heat = heat;
+  this.img = img;
+};
+
+//This is the list of peppers created as objects and set to variables (they are in alphabetical order)
+
+let pep01 = new pepper (
+  "Anaheim",
+  "California Chile, Magdalena, New Mexico Pepper, Chile Seco Del Norte",
+  "New Mexico", 
+  "500",
+  "5000",
+  "Mild",
+  null
+);
+
+let pep02 = new pepper(
+  "Bell Pepper",
+  "Sweet Pepper, Capsicum, Pepper",
+  "Mexico, Central America, and upper South America",
+  "0",
+  null,
+  "Sweet",
+  null
+);
+
+let pep03 = new pepper (
+ "Habanero",
+ "The Chinese Pepper, Caribbean Red, Chocolate Habanero",
+ "The Amazonas Region",
+ "100,000",
+ "350,000",
+ "HOT",
+  null
+);
+
+let pep04 = new pepper (
+  "Jalapeno",
+  "Huachinango, Chile Gordo",
+  "Xalapa, Veracruz",
+  "3,500",
+  "8,000",
+  "Medium",
+  null
+);
+
+let pep05 = new pepper (
+  "Pepperoncini",
+  "Peperone, Friggitello, Tuscan Pepper",
+  "Italy and Greece",
+  "0",
+  "500",
+  "Sweet",
+  null
+);
+
+let pep06 = new pepper (
+  "Poblano",
+  "Chile Ancho",
+  "Puebla, Mexico",
+  "1,000",
+  "2,500",
+  "Mild",
+  null
+);
+
+let pep07 = new pepper (
+  "Serrano",
+  null,
+  "Sierra Mountain Range, Mexico",
+  "10,000",
+  "25,000",
+  "Hot",
+  null
+);
+
+console.log(pep07);
+console.log(pep03);
+
+//List of jquery event listeners for each button/anchor_tag and their functions for displaying their info
+
+$("#pep01").click(function(){
+  $("#pep_Name").append(pep01.pep_Name);
+})
+
+//This is the search function for the main search page! (Might want to convert to Jquery later :\)
+
 document.getElementById("myInput").addEventListener(onkeyup, search_Function);
 
 function search_Function(){
@@ -29,11 +124,3 @@ for (i = 0; i < li.length; i++) {
    };
 }
 }
-
-
-fetch("./peppers.json")
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data));
-
